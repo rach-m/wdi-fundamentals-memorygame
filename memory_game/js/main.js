c2715e2461f12f3333f391e3e2c62d8db460746b
Rachel Moskowitz <rachelmoskowitz@Rachels-MBP-3.fios-router.home>
2018-04-12 17:11:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,30 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+
+{ 
+rank: "queen", 
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -15,14 +41,19 @@ var checkForMatch = function(){
 
 var flipCard = function(cardID){
 	
-	console.log("User flipped" + " " + cards[cardID])
-	cardsInPlay.push(cards[cardID])
+	console.log("User flipped" + " " + cards[cardID].rank)
+	cardsInPlay.push(cards[cardID].rank)
 
 	checkForMatch();
+
+	console.log(cards[cardID].cardImage)
+	console.log(cards[cardID].suit)
 
 	
 };
 
 
+
 flipCard(0);
 flipCard(1);
+
